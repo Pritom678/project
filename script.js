1b208cb853eadd8802da27e54c96b850e19c3644
Pritom678 <152401917+Pritom678@users.noreply.github.com>
2026-03-15 15:03:17 +0600

    footer contributor and FAQ section added

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const nav = document.querySelector(".nav-links");
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// FAQ toggle
+
+document.querySelectorAll(".faq-question").forEach((button) => {
+  button.addEventListener("click", () => {
+    const faqCard = button.parentElement;
+    faqCard.classList.toggle("active");
+
+    const icon = button.querySelector(".icon");
+    icon.textContent = faqCard.classList.contains("active") ? "-" : "+";
+  });
+});
